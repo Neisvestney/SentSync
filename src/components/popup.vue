2d083c0883edb70da794f104996ac76c3787275b
Neisvestney <neisvestney113@gmail.com>
2021-07-07 20:18:45 +0300

    Cool design

--- src/components/popup.vue
+++ src/components/popup.vue
@@ -1,23 +1,34 @@
 <template>
   <div id="main">
-    <input v-model="username" placeholder="Ваше имя">
+    <h1>SentSync</h1>
+    <div class="form-group">
+      <label>Имя</label>
+      <input v-model="username"/>
+      <!--      <button>Сохранить</button>-->
+    </div>
+
     <div v-if="!isConnected" class="connection">
-      <input v-model="room" placeholder="Комната">
-      <button @click="post({action: 'connect'})">Соедениться</button>
+      <div class="form-group">
+        <label>Комната</label>
+        <input v-model="room">
+        <button @click="post({action: 'connect'})">Соедениться</button>
+      </div>
 
     </div>
     <div v-else class="connection">
-      <p>Код комнаты: {{ room }}</p>
-      <p>Соеденено</p>
-      <button @click="post({action: 'disconnect'})">Отключиться</button>
+      <div class="form-group">
+        <label>Код комнаты</label>
+        <p>{{ room }}</p>
+        <button @click="post({action: 'disconnect'})" class="danger">Отключиться</button>
+      </div>
+
     </div>
     <br>
-    <div class="controls">
-      <button @click="post({action: 'pause'})">Пауза</button>
-      <button @click="post({action: 'play'})">Продолжить</button>
-      <input type="number" v-model="seekTo" placeholder="отредактируй меня">
-      <button @click="post({action: 'seek', to: seekTo})">Перейти</button>
-    </div>
+    <!--    <div class="controls">-->
+    <!--      <button @click="post({action: 'pause'})">Пауза</button>-->
+    <!--      <button @click="post({action: 'play'})">Продолжить</button>-->
+    <!--    </div>-->
+    <span class="footer">Made by <a target="_blank" href="https://github.com/Neisvestney">Neisvestney</a></span>
   </div>
 </template>
 
@@ -54,7 +65,7 @@ export default {
         this.post({action: 'setData', data: {room: newRoom}})
     }
   },
-  beforeMount: function(){
+  beforeMount: function () {
     this.port.onMessage.addListener((msg) => {
       console.log(msg);
       if (msg.data) {
